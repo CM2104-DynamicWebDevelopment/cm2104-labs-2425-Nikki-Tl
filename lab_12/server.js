2ccc1f0136af510df4c66865db93d1136555b6e5
Nikki-TL <n.tluczek@rgu.ac.uk>
2025-02-06 11:10:30 +0000

    11

--- lab_12/server.js
+++ lab_12/server.js
@@ -10,3 +10,22 @@ var spotifyApi = new SpotifyWebApi({
     clientSecret: '193db95c4caa4994b41e71069564db8c'
 
 });
+
+
+
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant().then( 
+    function (data) {
+        console.log('The access token expires in ' + data.body['expires_in']);
+        console.log('The access token is ' + data.body['access_token']);
+
+        // Save the access token so that it's used in future calls
+        spotifyApi.setAccessToken(data.body['access_token']); 
+    },
+    function (err) {
+        console.log(
+            'Something went wrong when retrieving an access token',
+            err.message 
+        );
+    }
+);
