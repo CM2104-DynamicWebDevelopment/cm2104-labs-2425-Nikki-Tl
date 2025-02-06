d0ef841889e01cbd0bb22d9b4bf897645259ebaa
Nikki-TL <n.tluczek@rgu.ac.uk>
2025-02-06 11:44:13 +0000

    1

--- lab_12/server.js
+++ lab_12/server.js
@@ -61,10 +61,12 @@ async function getTracks(searchterm, res) {
    
 
 
-//route for love in tracks, artists and albums
-app.get('/searchLove', function (req, res) {
-    getTracks('king von', res);
-});  
+//route for searching in tracks, artists and albums
+app.get('/search', function (req, res) {
+    var searchterm = req.query.searchterm;
+    getTracks(searchterm, res);
+});
+   
 
 
 
