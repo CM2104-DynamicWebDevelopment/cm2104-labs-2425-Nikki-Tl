478a03b346b54a9098982aba4f6aa2455060ba29
Nikki-Tl <nikolatluczek1@gmail.com>
2025-03-06 12:06:59 +0000

    1

--- lab_18/server.js
+++ lab_18/server.js
@@ -60,13 +60,13 @@ app.get('/', function(req, res) {
   
 
   var loggedinuserResult = "";
-  console.log("Something pase: "+ req.session.loggedinuser);
+  console.log("Something RAHHHHHH: "+ req.session.loggedinuser);
   //otherwise perfrom a search to return all the documents in the people collection
   db.collection('people').findOne({"login.username": req.session.loggedinuser}),function(err, result){
     if (err) throw err;
     //the result of the query is sent to the users page as the "users" array
     loggedinuserResult = result;
-    console.log("Something pase: "+result);
+    console.log("Something pase: "+ result);
     
   };
 
