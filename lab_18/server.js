4cb8e5af0fc40172178c350847361c4fc8cb305f
Nikki-Tl <nikolatluczek1@gmail.com>
2025-03-06 11:59:12 +0000

    1

--- lab_18/server.js
+++ lab_18/server.js
@@ -65,7 +65,7 @@ app.get('/', function(req, res) {
     if (err) throw err;
     //the result of the query is sent to the users page as the "users" array
     loggedinuserResult = result;
-    console.log(result);
+    console.log("Something pase: "+result);
     
   };
 
