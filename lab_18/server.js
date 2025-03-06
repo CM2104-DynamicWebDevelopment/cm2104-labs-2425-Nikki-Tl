4fab06d566ded218459c2d9e01df1dc9d5652092
Nikki-Tl <nikolatluczek1@gmail.com>
2025-03-06 11:22:44 +0000

    1

--- lab_18/server.js
+++ lab_18/server.js
@@ -57,7 +57,7 @@ app.get('/', function(req, res) {
   //if the user is not logged in redirect them to the login page
   if(!req.session.loggedin){res.redirect('/login');return;}
 
-  db.collection('people').findOne(uname),function(err, result){
+  db.collection('people').findOne({"login.username": uname}),function(err, result){
     if (err) throw err;
     //the result of the query is sent to the users page as the "users" array
     res.render('pages/users', {
